b5deab1c111a2bffda0a9851015492d37541e785
TetianaMurashova <murashovatania1@gmail.com>
2020-09-06 12:15:13 +1000

    6.09 L20

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,10 +1,12 @@
 
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?> >
 <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1" >
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
         <h1 class="school-logo-text float-left">
@@ -15,11 +17,11 @@
         <div class="site-header__menu group">
             <nav class="main-navigation">
                 <ul>
-                    <li><a href="<?= site_url('/about-us') ?>">About Us</a></li>
+                    <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 11) echo 'class="current-menu-item"'?> ><a href="<?= site_url('/about-us') ?>">About Us</a></li>
                     <li><a href="#">Programs</a></li>
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Campuses</a></li>
-                    <li><a href="#">Blog</a></li>
+                    <li><a href="<?= site_url('/blog') ?>">Blog</a></li>
                 </ul>
             </nav>
             <div class="site-header__util">
